8dbf21827523ecc90de845b523416faa2c10fcb3
ann-leonard <aleo0779@gmail.com>
2019-07-18 00:45:21 -0400

    nav fixes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 import {Navbar, NavDropdown, Nav, Form, FormControl, Button} from 'react-bootstrap';
 import Axios from 'axios';
-import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
 import { Redirect } from 'react-router-dom'
 class Navigation extends Component {
 
@@ -14,13 +15,13 @@ class Navigation extends Component {
         return (<div>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Navbar.Brand href="/user/dashboard">            
-                <img src="https://www.pngkey.com/png/detail/522-5227751_stocks-icon-png-stocks-icon.png" className="icon rounded-pill" alt=""/>
+                <img src="https://www.pngkey.com/png/detail/522-5227751_stocks-icon-png-stocks-icon.png" className="icon m-3 rounded-pill" alt=""/>
                 StockUp
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-
+                    <Nav className="mr-auto ml-5 ">
+                        <Link to="/user/dashboard" component={Dashboard}>Dashboard</Link>
                     </Nav>
                     <NavDropdown inline title="Account" id="basic-nav-dropdown">
                             <NavDropdown.Item onClick={this.handleLogout}>Log Out</NavDropdown.Item>
